65020f17ed951a48e1924f8ed57c407546243010
JoJo <apple@apples-MacBook-Pro.local>
2018-10-29 21:18:36 -0700

    first file

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const statesNumber = 50;
+
+var add = 5 + 4;
+
+var name = 'Joseph';
+var startName= name[0];
+index = 76;
+
+if(name.charCodeAt(startName) >  index){
+    console.log('Back of the line!')
+} else{
+    console.log('Next!');
+}
