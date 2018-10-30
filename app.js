b25d86cebbcfd7129544b90471b9e7e54ec0fa7a
JoJo <apple@apples-MacBook-Pro.local>
2018-10-29 21:29:17 -0700

    Second Function added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 
+// 3. Create a variable that contains your name as a value. Name the variable appropriately.
+var name = 'Joseph';
+
+// 4. Create a constant that contains the number of states in the U.S. and name it appropriately.
 const statesNumber = 50;
 
+// 5.Compute the result of adding 5 and 4 and store it in an appropriately named variable.
 var add = 5 + 4;
 
-var name = 'Joseph';
+// 6. Write some code so that if your name starts with a letter after L, you display an alert that says "Back of the line!". Otherwise, you should display an alert that says "Next!".
 var startName= name[0];
 index = 76;
 
@@ -12,3 +17,10 @@ if(name.charCodeAt(startName) >  index){
 } else{
     console.log('Next!');
 }
+
+// 8. Write a function called sayHello that displays an alert that says Hello World!
+function sayHello(){
+    alert('Hello World!');
+}
+// 9.Call the sayHello function.
+sayHello();
